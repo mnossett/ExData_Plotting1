33b570ebb1ea02b9850aa96eb44423a8fde497db
mnossett <mnossett@yahoo.com>
2023-11-03 14:40:52 -0400

    added full code

--- plot1.R
+++ plot1.R
@@ -1,3 +1,21 @@
+# Martin Nossett
+# Exploratory Data Analysis Course Project 1
+
+## DOWNLOAD AND SAVE RAW DATA TO "raw_data" FOLDER
+zipurl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(zipurl, destfile="./raw_data/ExData.zip", mode="wb")
+unzip(zipfile = "./raw_data/ExData.zip", exdir = "./raw_data/")
+
+          #  read file for data set
+setwd("./raw_data")
+RawData <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+dim(RawData)   # 2,075,259 rows x 9 columns
+
+          #  subset to only show data between dates 2007-02-01 and 2007-02-02
+fix_date <- mutate(RawData, Date = strptime(RawData$Date, format="%d/%m/%Y", tz=""))
+ExData <- subset(fix_date, Date >= '2007-02-01' & Date <= '2007-02-02')
+
+
 ## CREATE PLOT 1
 png(filename = "./plot1.png")
 hist(ExData$Global_active_power, col= "red", main= "Global Active Power", 
